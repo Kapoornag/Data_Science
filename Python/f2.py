7fa8b0007a3e4dfe1bb11831c502bbc270a0cc1a
Kapoor Chand Nag <kapoornag@gmail.com>
2023-07-16 19:21:05 +0530

    My first commit & push to cloud repository

---
+++ Python/f2.py
@@ -0,0 +1,3 @@
+Farmers are the backbone of the economy. accounting for more than half of India's population Dependent on agriculture as source of income.
+For the survival of the Peasant world at the beginning of the nation or the beginning of civilization. It can be said that he was the most distinguished person who came. Agriculture is the only means of livelihood for the farmers.
+Farmers are the soul of the nation and constiture almost two-thirds of the working class in India.
